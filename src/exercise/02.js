83e0d454f926a072719d451bba099115e9b60313
Diijon Taylor <diijon@gmail.com>
2021-10-21 08:27:25 -0400

    finished 02: useEffect

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -1,16 +1,52 @@
 // useEffect: persistent state
 // http://localhost:3000/isolated/exercise/02.js
 
-import * as React from 'react'
+import React, { useState, useEffect } from 'react'
 
-function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
+function useLocalStorageState(itemName, itemDefaultValue = ''){
+  const [item, setItem] = useState(() => {
+    return window.localStorage.getItem(itemName) || itemDefaultValue
+  })
 
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
+  useEffect(() => {
+    window.localStorage.setItem(itemName, item)
+  }, [itemName, item])
+
+  return [item, setItem]
+}
+
+//eslint-disable-next-line
+function Greeting({initialName = ''}) {
+  // 🐨 initialize the state to the value from localStorage
+  // 💰 window.localStorage.getItem('name') || initialName
+  const [name, setName] = useState(() => {
+    return window.localStorage.getItem('name') || initialName
+  })
+
+  // 🐨 Here's where you'll use `React.useEffect`.
+  // The callback should set the `name` in localStorage.
+  useEffect(() => {
+    window.localStorage.setItem('name', name)
+  }, [name])
+
+  function handleChange(event) {
+    setName(event.target.value)
+  }
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input value={name} onChange={handleChange} id="name" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+    </div>
+  )
+}
+
+function GreetingExtraCredit3And4({initialName = ''}) {
+  const [name, setName] = useLocalStorageState('name', initialName)
 
   function handleChange(event) {
     setName(event.target.value)
@@ -27,7 +63,7 @@ function Greeting({initialName = ''}) {
 }
 
 function App() {
-  return <Greeting />
+  return <GreetingExtraCredit3And4 initialName="dude" />
 }
 
 export default App
